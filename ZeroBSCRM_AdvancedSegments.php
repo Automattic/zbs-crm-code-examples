825c04b323bf2787addc6ef3aacabad0c457a2d9
woodyhayday <woody@stormgate.co.uk>
2018-07-04 11:36:41 +0100

    improved, moved parent class to dal

--- ZeroBSCRM_AdvancedSegments.php
+++ ZeroBSCRM_AdvancedSegments.php
@@ -23,50 +23,6 @@ function zeroBSCRM_advSegmentConditions(){
 } add_action('admin_init','zeroBSCRM_advSegmentConditions',1);
 
 
-/*
- * This Parent class allows us to simplify what's needed of each condition into a class below
-*/
-class zeroBSCRM_segmentCondition {
-
-    public $key = false;
-    public $condition = false;
-
-
-    /**
-     * Zero BS CRM Segment Argument Constructor.
-     */
-    public function __construct() {
-
-        global $zbs;
-
-        if ($this->key !== false && is_array($this->condition)){
-
-            // add the condition
-            add_filter( 'zbs_segment_conditions', array($this,'condition'));
-
-            // add the query arg builder
-            add_filter( $zbs->DAL->makeSlug($this->key).'_zbsSegmentArgumentBuild', array($this,'conditionArg'),10,2);
-
-        }
-
-    }
-
-    public function condition($conditions=array()) {
-
-        if ($this->key !== false && is_array($this->condition)) return array_merge($conditions,array($this->key => $this->condition));
-
-        // else don't add
-        return $conditions;
-    }
-    // note starting arg is ignored (should not have been called multiple times)
-    public function conditionArg($startingArg=false,$condition=false,$conditionKeySuffix=false){
-
-        global $zbs,$wpdb,$ZBSCRM_t;
-
-        return $startingArg;
-    }
-
-}
 
 /* ====================================================================================
  * The following classes each represent a possible segment condition
